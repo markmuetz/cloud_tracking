e89a89d4d928f2dcb433642c23eeff37c5a4cddb
Mark Muetzelfeldt <markmuetz@gmail.com>
2018-06-20 11:20:28 +0100

    Add matplotlib to requires.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='cloud_tracking',
-    version='0.1.1',
+    version='0.1.2',
     packages=['cloud_tracking'],
-    url='',
+    url='https://github.com/markmuetz/cloud_tracking',
     license='',
     author='markmuetz',
     author_email='m.muetzelfeldt@pgr.reading.ac.uk',
     description='Simple cloud tracking',
-    requires=['numpy', 'iris'],
+    requires=['numpy', 'iris', 'matplotlib'],
     scripts=['bin/track_clouds'],
 )
